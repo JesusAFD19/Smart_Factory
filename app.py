4531dd43c18b1064b7ae5507d467b22324183a0d
ale_wsl <alejo.ubeto@gmail.com>
2023-03-06 01:10:27 -0600

    send mqtt request function added

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # Librerías
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import paho.mqtt.client as mqtt
 
 
 # Nueva instancia de Flask
@@ -7,9 +8,39 @@ app = Flask(__name__)
 
 
 # Funciones
-@app.route('/')
+def send_mqtt(topic, message):
+    client = mqtt.Client()
+    #broker_address = "34.125.48.250"
+    client.connect("broker.mqttdashboard.com", 1883, 60)
+
+    # Cliente MQTT debe esperar una confirmación del broker MQTT
+    sent = False
+    def on_publish(client, userdata, mid):
+        nonlocal sent
+        sent = True
+    client.on_publish = on_publish
+
+    # Publicar el mensaje y esperar una confirmación
+    client.publish(topic, message)
+    while not sent:
+        client.loop()
+
+    client.disconnect()
+    print(f'MQTT => Petición {topic} enviada')
+
+
+# Renderización de index como página principal
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    # Lista de opciones a renderizar
     list = ['Bird','Cat','Fish','House','Plane','Rocket','Swan','Tree']
+
+    # Si se envía el formulario de index.html
+    if request.method == 'POST':
+        picture_selection = request.form['picture_selection']
+        send_mqtt(picture_selection,picture_selection)
+
+    # Renderizamos index.html pasandole la lista de figuras
     return render_template('index.html', list=list)
 
 
