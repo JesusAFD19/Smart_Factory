cde9672ff2080af84675406c95b295e21dbff635
ale-gitbash <alejo.ubeto@gmail.com>
2023-05-16 20:00:47 -0600

    [FIXED] clickable card and content DONE

--- frontend/src/components/CustomCard.jsx
+++ frontend/src/components/CustomCard.jsx
@@ -1,8 +1,14 @@
 /* Componente base de Tarjeta */
 
 export const CustomCard = (props) => {
+  const handleOnClick = (event) => {
+    const parentId = event.currentTarget.id;
+    if (typeof props.handleClick === 'function') {
+      props.handleClick(parentId);
+    }
+  };
   return (
-    <section className={props.customClass} draggable={props.draggable} onDragStart={props.onDragStart}>
+    <section id={props.id} className={props.customClass} onClick={handleOnClick} draggable={props.draggable} onDragStart={props.onDragStart}>
       <div className="card-image">{props.cardImage}</div>
       <div className="card-content">{props.content}</div>
       <div className="card-footer">{props.footer}</div>
