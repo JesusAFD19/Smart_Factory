49e226f2efdf27e2efc4fc9004d40b44d2cbdc21
ale_wsl <alejo.ubeto@gmail.com>
2023-05-30 02:17:26 -0600

    [ROUTE] link to form page added

--- frontend/src/routes/LandingPage.jsx
+++ frontend/src/routes/LandingPage.jsx
@@ -1,11 +1,16 @@
 import { CustomNavbar } from '../components/CustomNavbar'
+import { CustomButton } from '../components/CustomButton'
 import { Footer } from '../components/Footer'
+import { Link } from 'react-router-dom';
 
 export const LandingPage = () => {
   return (
     <section id='Landing'>
       <CustomNavbar/>
-      <div>LandingPage</div>
+      <div>
+        <h2>landing page</h2>
+        <Link to='/build'><CustomButton text={"Empieza ahora"}/></Link>
+      </div>
       <Footer/>
     </section>
   )
